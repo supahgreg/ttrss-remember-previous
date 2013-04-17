1b86dd5f66ab593a664bba3fb9713e626e6f4820
supahgreg <greg.parris@gmail.com>
2013-04-16 22:49:58 -0500

    First release

---
+++ README.md
@@ -0,0 +1,13 @@
+Remember Previous
+=================
+
+Remember your last-viewed category or feed.
+
+The data is currently saved in a cookie, and is therefore not carried across browsers/computers.
+
+A [Tiny Tiny RSS](http://www.tt-rss.org) plugin.
+
+Installation
+------------
+1. Copy the `remember_previous` directory to the `plugins` directory of your Tiny Tiny RSS installation.
+2. Go to Preferences / Plugins and enable `remember_previous`.
---
+++ remember_previous/init.php
@@ -0,0 +1,53 @@
+<?php
+class Remember_Previous extends Plugin {
+
+	private $link;
+	private $host;
+
+
+	function about() {
+		return Array(
+        1.0 // version
+      , "Remember your last-viewed category or feed." // description
+      , "wn" // author
+      , false // is system
+      , "https://www.github.com/supahgreg/ttrss-previous-previous" // more info URL
+    );
+	}
+
+
+	function init($host) {
+		//$this->link = $host->get_link();
+		//$this->host = $host;
+	}
+  
+  function get_js() {
+    return <<<'JS'
+;(function(aSetActiveFeedId) {
+  var oldSetActiveFeedId = aSetActiveFeedId
+    , COOKIE_NAME = "remember_previous"
+    , prev
+    ;
+
+  // Wrap the original setActiveFeedId so we can remember
+  function _setActiveFeedId(aId, aIsCategory) {
+    setCookie(COOKIE_NAME, aId + "," + (aIsCategory ? 1 : 0), 604800); // 1 week
+    return oldSetActiveFeedId.call(null, aId, aIsCategory);
+  }
+
+  window.setActiveFeedId = _setActiveFeedId;
+
+  prev = getCookie(COOKIE_NAME);
+  if (!(prev && /^-?\d+,[01]$/.test(prev)))
+    return;
+
+  console.log("restoring category or feed: " + prev);
+  prev = prev.split(",");
+  hash_set("f", prev[0]);
+  hash_set("c", prev[1]);
+})(setActiveFeedId);
+JS;
+  }
+}
+?>
+
